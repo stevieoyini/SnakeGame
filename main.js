a439710cc785b542add719e6d8a150db6d25b233
Stevie Oyini <steevoyini@yahoo.fr>
2022-01-28 00:21:48 +0100

    new feature

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 //target Canvas
 
-let canvas = document.getElementById("canvas")
+let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext('2d');
 //Variable
 //speed in x
@@ -9,6 +9,13 @@ vx = 10;
 
 //speed in y 
 vy = 0;
+
+//AppleX
+let appleX = 0;
+
+//AppleY
+let appleY = 0;
+
 
 
 /*we draw the snake*/
@@ -19,7 +26,13 @@ function animation(){
 
         CleanCanvas();
 
+        DrawApple();
+
         MoveTheSnakeForward();
+        
+        if(EndGame()){
+           return;
+        }
 
         DrawTheSnake();
 
@@ -30,16 +43,17 @@ function animation(){
 
 }
 animation();
+ToCreateApple();
 
 function CleanCanvas(){
     //we draw the square
 
-ctx.fillStyle = "white";
+ctx.fillStyle = "blue";
 ctx.strokeStyle = "black";
-ctx.fillRect(0,0,canvas.width,canvas.height);
+ctx.fillRect(0,0,canvas.width, canvas.height);
 
 //outline
-ctx.strokeRect(0,0,canvas.width,canvas.height);
+ctx.strokeRect(0,0,canvas.width, canvas.height);
 
 }
 
@@ -65,8 +79,16 @@ function MoveTheSnakeForward(){
     const head = {x: snake[0].x + vx, y: snake[0].y + vy};
     //Add an element at the beginning of the table in javascript we use the method .unshift
     snake.unshift(head);
+
+    const SnakeEatApple = snake[0].x === appleX && snake[0].y === appleY;
+
     // Remove the last element of an array we use the method .pop.
-    snake.pop();
+
+    if(SnakeEatApple) {
+        ToCreateApple();
+    } else {
+        snake.pop();
+    }
 
 }
 
@@ -97,3 +119,58 @@ if(direction === ARROW_DOWN && !top) { vx = 0; vy = 10; }
 
 }
 
+function random(){
+
+    return Math.round((Math.random() * 390) / 10) * 10;
+}
+
+
+function ToCreateApple(){
+
+    appleX = random();
+    appleY = random();
+    //console.log(appleX,appleY);
+    
+    snake.forEach(function(part){
+
+        const SnakeOnApple = part.x == appleX && part.y == appleY;
+
+        if(SnakeOnApple){
+            ToCreateApple();
+        }
+    })
+}
+
+function DrawApple(){
+
+    ctx.fillStyle = 'red';
+    ctx.strokeStyle = "darkred";
+    ctx.beginPath();
+    ctx.arc(appleX + 5, appleY  + 5, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function EndGame(){
+
+    let SnakeHeadless = snake.slice(1,-1);
+    let bitten = false;
+    SnakeHeadless.forEach(pieces =>{
+        if(pieces.x === snake[0].x && pieces.y === snake[0].y){
+            bitten = true;
+        }
+    })
+
+    const LeftWallKey = snake[0].x < -1;
+    const RightWallKey = snake[0].x > canvas.width - 10;
+    const TopWallKey= snake[0].y < 1;
+    const BottomtWallKey = snake[0].y > canvas.height -10;
+    
+    let gameOver = false;
+    
+    if(bitten || LeftWallKey || RightWallKey || TopWallKey || BottomtWallKey) {
+        gameOver = true;
+    };
+
+    return gameOver;
+}
